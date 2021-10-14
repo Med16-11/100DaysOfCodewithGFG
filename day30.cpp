99e7fc020f169c6231085445b2e4069ff79ca976
Medhavi Singh <78318301+Med16-11@users.noreply.github.com>
2021-10-14 11:34:41 +0530

    Create day30.cpp

---
+++ day30.cpp
@@ -0,0 +1,12 @@
+//Swap all odd and even bits 
+unsigned int swapBits(unsigned int n)
+    {
+    	unsigned int even_bits = n & 0xAAAAAAAA;
+
+        unsigned int odd_bits = n & 0x55555555;
+ 
+        even_bits >>= 1; 
+        odd_bits <<= 1; 
+ 
+        return (even_bits | odd_bits);
+    }
